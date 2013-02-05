58c4192a11546cda00468cc8a993832c04dc3646
Dmitry Kurilo <me@masterrr.name>
2013-02-05 21:04:46 +0300

    semicolon delete, save without brackets

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,10 +1,10 @@
 class PostController < ApplicationController
 	def index
-		@posts = Post.all;
+		@posts = Post.all
 	end
 	def create
 		current_local_ip = request.env['REMOTE_ADDR']
-		Post.new(:title => params[:title], :bodytext => params[:bodytext], :ip => current_local_ip).save();
+		Post.new(:title => params[:title], :bodytext => params[:bodytext], :ip => current_local_ip).save;
 		redirect_to(:back)
 	end
 
