014263a20c25e8cebd7cc223115426f783a96ae0
Dmitry Kurilo <me@masterrr.name>
2013-02-05 17:43:27 +0300

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsPublicBlog::Application.config.secret_token = 'c3e58fe2ee8544cfa0367d7c547b8a07052a69b33dfc98120aabfdd7b3ff04b83411b688ed877966b59fa0d55cb63408686b738367a7ac9dac020a8e59205dcb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailsPublicBlog::Application.config.session_store :cookie_store, key: '_rails-public-blog_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RailsPublicBlog::Application.config.session_store :active_record_store
