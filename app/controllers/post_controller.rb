4130a91a18f2976348d5a2630f0d25a6fa5e6c5f
Dmitry Kurilo <me@masterrr.name>
2013-02-05 21:31:32 +0300

    limit to 15

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,6 +1,6 @@
 class PostController < ApplicationController
 	def index
-		@posts = Post.order('created_at DESC').limit(1)
+		@posts = Post.order('created_at DESC').limit(15)
 	end
 	def create
 		current_local_ip = request.env['REMOTE_ADDR']
