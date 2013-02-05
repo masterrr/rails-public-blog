79ece05a22296e5b544b65f873ddfbaab56eb05e
Dmitry Kurilo <me@masterrr.name>
2013-02-05 21:29:45 +0300

    css, post query, validate post, index layout changes

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,6 +1,6 @@
 class PostController < ApplicationController
 	def index
-		@posts = Post.all
+		@posts = Post.order('created_at DESC').limit(1)
 	end
 	def create
 		current_local_ip = request.env['REMOTE_ADDR']
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :bodytext, :ip, :title
+
+  validates :bodytext, :ip, :title, :presence => true
+  validates :title, :length => { :maximum => 30}
+  validates :bodytext, :length => { :maximum => 500 }
 end
