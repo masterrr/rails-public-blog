a40756890721e718249e8510e9ab1c6b84c6eeb8
Dmitry Kurilo <me@masterrr.name>
2013-02-06 17:30:06 +0300

    space idents, new hash syntax, deleted semicolon

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,15 +1,15 @@
 class PostController < ApplicationController
-	def index
-		@posts = Post.order('created_at DESC').limit(15)
-		if request.xhr?
-			render :partial => "postlist"
-		else
-			render :index
-		end
-	end
+  def index
+    @posts = Post.order('created_at DESC').limit(15)
+    if request.xhr?
+      render :partial => "postlist"
+    else
+      render :index
+    end
+  end
 
-	def create
-		Post.new(:title => params[:title], :bodytext => params[:bodytext], :ip => request.ip).save;
-	end
+  def create
+    Post.create(title: params[:title], bodytext: params[:bodytext], ip: request.ip)
+  end
 
 end
